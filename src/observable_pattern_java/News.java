d7ca6f272791e8a08cf952ab759ba77bfcb64d8f
junaideqbal <junaid4238@gmail.com>
2020-11-08 12:39:27 +0500

    Observable Pattern

---
+++ src/observable_pattern_java/News.java
@@ -0,0 +1,19 @@
+package observable_pattern_java;
+
+import java.util.Observable;
+
+public class News extends Observable{
+
+	public void news() {
+		// TODO Auto-generated method stub
+		String[] news = {"News 1", "News 2", "News 3"};
+        for(String s: news){
+            //set change
+            setChanged();
+            //notify observers for change
+            notifyObservers(s);
+            
+        }
+
+	}
+}
---
+++ src/observable_pattern_java/SecondNewsReader.java
@@ -0,0 +1,15 @@
+package observable_pattern_java;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class SecondNewsReader implements Observer{
+
+	@Override
+	public void update(Observable o, Object arg) {
+		// TODO Auto-generated method stub
+		System.out.println("SecondNewsReader got The news:"+(String)arg);
+		
+	}
+
+}
